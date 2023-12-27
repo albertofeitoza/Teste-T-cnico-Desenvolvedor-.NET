b7c5aba506e78ec1bcad93ddc3079b6124b8d955
Alberto <alberto.feitoza@outlook.com>
2023-12-27 12:37:40 -0300

    Criado DbContext

---
+++ Data/TesteTecnicoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teste_Tecnico_Desenvolvedor_.NET.Data
+{
+    public class TesteTecnicoDbContext : DbContext
+    {
+        public TesteTecnicoDbContext(DbContextOptions<TesteTecnicoDbContext> options)
+           : base(options) { }
+
+        
+    }
+}
