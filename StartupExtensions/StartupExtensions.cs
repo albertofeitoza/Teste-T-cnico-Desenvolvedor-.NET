4d784983af0ed269b66919c4aeb4e8c9ca49c26b
Alberto <alberto.feitoza@outlook.com>
2023-12-27 12:38:25 -0300

    Criado arquitetura de Configurações e injeções de dependencias

---
+++ StartupExtensions/StartupExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Teste_Tecnico_Desenvolvedor_.NET.StartupExtensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddServiceDependency(WebApplicationBuilder builder)
+        {
+            ImplementcaoInterfaces(builder.Services);
+            ImplementcaoRepositorios(builder.Services);
+            ImplementacaoBancoDados(builder);
+        }
+
+        private static void ImplementacaoBancoDados(WebApplicationBuilder builder)
+        {
+            builder.Services
+                .AddDbContext<Data.TesteTecnicoDbContext>(options => 
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("SQL")));
+        }
+
+        private static void ImplementcaoRepositorios(IServiceCollection services)
+        {
+
+        }
+
+        private static void ImplementcaoInterfaces(IServiceCollection services)
+        {
+
+
+
+        }
+    }
+}
